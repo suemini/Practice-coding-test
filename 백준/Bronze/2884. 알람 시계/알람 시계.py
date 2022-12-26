7e2eea28c11b78b40966a90170d98d19335ad0e7
suemini <121214637+suemini@users.noreply.github.com>
2022-12-26 16:25:38 +0900

    [Bronze III] Title: 알람 시계, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,10 +1,12 @@
 H, M = map(int, input().split())
-if 0 < H <= 23 and 0 <= M < 45:
-	print(H-1, M+15)
-elif 0 < H <= 23 and 45 <= M <= 59:
-	print(H, M-45)
-elif 0 <= H and 0 <= M < 45:
-	print(23, M+15)
-elif 0 <= H and 45 <= M <= 59:
-	print(H, M-45)
-    
+if M < 45:
+	H = H - 1
+	M = M + 60
+	M = M - 45
+elif M >= 45:
+	M = M - 45
+
+if H < 0:
+	H = 23
+
+print(H, M)
