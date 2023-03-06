9a70a30d5377816e736a4029779a692eafe64d9e
suemini <121214637+suemini@users.noreply.github.com>
2023-03-06 18:23:37 +0900

    [level 0] Title: 대문자와 소문자, Time: 15.73 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,21 +1,14 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] my_stringArr = String.valueOf(my_string).split("");
-        String[] alphabetArr1 = new String[26];
-        String[] alphabetArr2 = new String[26];
-
-        for (int j = 0; j < my_stringArr.length; j++) {
-            for (int i = 0; i < alphabetArr1.length; i++) {
-                alphabetArr1[i] = String.valueOf((char) (97 + i));
-                alphabetArr2[i] = String.valueOf((char) (65 + i));
-                if (my_stringArr[j].equals(alphabetArr1[i])) {
-                    my_stringArr[j] = alphabetArr2[i];
-                } else if (my_stringArr[j].equals(alphabetArr2[i])) {
-                    my_stringArr[j] = alphabetArr1[i];
-                }
+               for (int i= 0; i < my_string.length(); i++) {
+            if(my_string.charAt(i)>=97 && my_string.charAt(i)<=122){
+                answer += (char)(my_string.charAt(i)-32);
+            } else if (my_string.charAt(i)>=65 && my_string.charAt(i)<=90) {
+                answer += (char) (my_string.charAt(i) + 32);
+            } else {
+                answer += my_string.charAt(i);
             }
-            answer = String.join("", my_stringArr);
         }
         return answer;
     }
