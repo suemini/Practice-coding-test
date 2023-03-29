b5a42a760ba3326b2a64bb4dff41b586089ac5e7
suemini <121214637+suemini@users.noreply.github.com>
2023-03-30 02:01:31 +0900

    [level 0] Title: 자릿수 더하기, Time: 2.40 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,13 +1,11 @@
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int[] nArr = Stream.of(String.valueOf(n).split(""))
-            .mapToInt(Integer::parseInt).toArray();
-        for(int i = 0; i<nArr.length; i++) {
-            answer += nArr[i];
-        }
+        int answer = Arrays.stream(Stream.of(String.valueOf(n)
+            .split("")).mapToInt(Integer::parseInt)
+            .toArray()).sum();
         return answer;
     }
 }
